a25ae656d7b9cb16a9afac9113d15e57a0b10dcc
Václav Macinka <xmacinka@gmail.com>
2013-05-07 04:20:30 +0200

    shared space plugin

--- vendor/assets/javascripts/ckeditor/build-config.js
+++ vendor/assets/javascripts/ckeditor/build-config.js
@@ -92,7 +92,8 @@ var CKBUILDER_CONFIG = {
 		'fakeobjects' : 1,
 		'richcombo' : 1,
 		'listblock' : 1,
-		'menubutton' : 1
+		'menubutton' : 1,
+		'sharedspace' : 1
 	},
 	languages : {
 		'en' : 1,
